98213c99fa0f9ee2fa27cb67a9575c24a0ecedc4
Sergey Gychka <sergey.gychka@gmail.com>
2017-01-12 14:38:43 +0200

    refactor GO/ObjectPool

--- Go/Creational/ObjectPool/main.go
+++ Go/Creational/ObjectPool/main.go
@@ -1,80 +1,13 @@
 package main
 
 import (
-	"errors"
 	"sync"
-	"fmt"
 	"time"
 	"math/rand"
+	"github.com/MastersAcademy/ma_ap_2016/Go/Creational/ObjectPool/pool"
 )
 
-type ImageProcessor struct {
-	id int
-}
 
-func (i *ImageProcessor)Process(data int) {
-	fmt.Printf("Processor %v process %v\n", i.id, data)
-}
-
-func NewImageProcessor(id int) *ImageProcessor{
-	return &ImageProcessor{
-		id: id,
-	}
-}
-
-var NoFreeProcessorError = errors.New("No Free processors")
-var instance *ProcessorPool
-var once sync.Once
-
-type ProcessorPool struct {
-	m sync.RWMutex
-	idle map[int]*ImageProcessor
-	used map[int]*ImageProcessor
-	maxProcessors int
-}
-
-func GetProcessorPool() *ProcessorPool {
-	once.Do(func() {
-		instance = &ProcessorPool{
-			idle: make(map[int]*ImageProcessor),
-			used: make(map[int]*ImageProcessor),
-			maxProcessors: 5,
-		}
-	})
-	return instance
-}
-
-func (p *ProcessorPool)Acquire() (*ImageProcessor, error) {
-	p.m.Lock()
-	defer p.m.Unlock()
-	if len(p.idle) == 0 && len(p.used) == p.maxProcessors {
-		return nil, NoFreeProcessorError
-	}
-
-	if len(p.idle) > 0 {
-
-		var processor *ImageProcessor
-		var k int
-		for k, processor = range p.idle {
-			delete(p.idle, k)
-			p.used[k] = processor
-			return processor, nil
-		}
-	}
-	id := len(p.idle) + len(p.used) + 1
-	obj := NewImageProcessor(id)
-	p.used[id] = obj
-	return obj, nil
-
-}
-
-func (p *ProcessorPool)Release(obj *ImageProcessor) {
-	p.m.Lock()
-	defer p.m.Unlock()
-	delete(p.used, obj.id)
-	p.idle[obj.id] = obj
-
-}
 
 func main(){
 	workersCount := 10
@@ -92,14 +25,13 @@ func main(){
 			for {
 				select{
 				case w := <-c:
-					processor, _ := GetProcessorPool().Acquire()
+					processor, _ := pool.GetProcessorPool().Acquire()
 					for processor == nil{
 						time.Sleep(time.Duration(rand.Intn(10)))
-						processor, _ = GetProcessorPool().Acquire()
+						processor, _ = pool.GetProcessorPool().Acquire()
 					}
 					processor.Process(w)
-					GetProcessorPool().Release(processor)
-					//time.Sleep(time.Duration(rand.Intn(10)))
+					pool.GetProcessorPool().Release(processor)
 				default:
 					return
 				}
